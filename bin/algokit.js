3c82b73539d834a370a6a2055a76396762f39e1a
Michael J Feher <github@phearzero.com>
2024-02-16 13:56:29 -0600

    feat: always download locally

--- bin/algokit.js
+++ bin/algokit.js
@@ -6,7 +6,6 @@ const REPO = 'https://github.com/algorandfoundation/algokit-cli'
 
 import os from 'node:os';
 import {resolve, dirname} from 'node:path'
-import commandExists from 'command-exists';
 import {spawn} from "node:child_process";
 import {Readable} from 'node:stream';
 import {finished} from 'node:stream/promises';
@@ -23,28 +22,25 @@ const SYSTEMS = {
     'Linux': 'Linux'
 }
 
-await commandExists('algokit', async (err, foundCommand)=>{
+let localCmd = resolve(__dirname, `algokit${os.type() === 'Windows_NT' ? '.exe' : ''}`)
+let cmdPath = existsSync(localCmd) ? localCmd : 'algokit'
 
-    if(err) throw err;
+// Run Install
+if(!existsSync(localCmd)) {
+    console.log('Installing algokit...')
+    // Download
+    const {body} = await fetch(`${REPO}/releases/download/${VERSION}/algokit-${VERSION}-${SYSTEMS[os.type()]}-py${PYTHON}.tar.gz`)
+    const fileStream = createWriteStream(resolve(__dirname, 'algokit.tar.gz'))
+    await finished(Readable.from(body).pipe(fileStream))
 
-    let localCmd = resolve(__dirname, `algokit${os.type() === 'Windows_NT' ? '.exe' : ''}`)
-    let cmdPath = existsSync(localCmd) ? localCmd : 'algokit'
+    // Unpack
+    const tarPath = resolve(__dirname, 'algokit.tar.gz')
+    await tar.x({
+        file: tarPath,
+        cwd: __dirname
+    })
+    unlinkSync(tarPath)
+    cmdPath = localCmd
+}
 
-    // Run Install
-    if(!foundCommand && !existsSync(localCmd)) {
-        // Download
-        const {body} = await fetch(`${REPO}/releases/download/${VERSION}/algokit-${VERSION}-${SYSTEMS[os.type()]}-py${PYTHON}.tar.gz`)
-        const fileStream = createWriteStream(resolve(__dirname, 'algokit.tar.gz'))
-        await finished(Readable.from(body).pipe(fileStream))
-
-        // Unpack
-        const tarPath = resolve(__dirname, 'algokit.tar.gz')
-        await tar.x({
-          file: tarPath,
-          cwd: __dirname
-        })
-        unlinkSync(tarPath)
-    }
-
-    spawn(cmdPath, process.argv.slice(2), { stdio: 'inherit' })
-})
+spawn(cmdPath, process.argv.slice(2), { stdio: 'inherit' })
